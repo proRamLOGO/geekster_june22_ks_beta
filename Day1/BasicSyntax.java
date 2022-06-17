3b411bddf11a4a4ac8782ad054c3941f1a7dd36f
Shubh Bansal <00shubhbansal@gmail.com>
2022-06-17 23:56:31 +0530

    Create BasicSyntax.java

---
+++ Day1/BasicSyntax.java
@@ -0,0 +1,46 @@
+import java.io.*;
+public class BasicSyntax {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Hello Shubh");
+        System.out.println(56);
+        System.out.println('?');
+        System.out.println('@');
+        System.out.println("5d3");
+        System.out.println("56");
+        System.out.println('5');
+        System.out.println("5");
+        System.out.println(5);
+    
+        
+        int x;
+        x = 50000;
+        System.out.println(x);
+        
+        int y = 45;
+        System.out.println(y);
+        
+        int n1 = 1, n2 = 2, n3 = 4;
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        
+        
+        
+        
+        
+        
+        String name;
+        name = input.nextLine();
+        
+        int num;
+        num = input.nextInt();
+        
+        float numf;
+        numf = input.nextFloat();
+        
+       
+    }
+}
